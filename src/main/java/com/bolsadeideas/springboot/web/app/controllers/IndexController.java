43fd067b38e9bdbf17bb7284b1913b32b2143e79
leanysp <leanysp@gmail.com>
2019-08-14 17:37:38 -0500

    parametros

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -35,12 +37,19 @@ public class IndexController {
 	}
 	
 	@RequestMapping("/listar")
-	public String listar(Model model) {
-		List<String> usuarios = new ArrayList();
-		
+	public String listar(Model model) {		
 		model.addAttribute("titulo", "Listado de usuarios: ");
-		model.addAttribute("usuarios", usuarios);
 		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuario(){
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Leanys", "Pineda", "leanysp@gmail.com"),
+				new Usuario("Dariel", "Pineda", "darielp@gmail.com"),
+				new Usuario("Oscar", "Pineda", "oscarp@gmail.com"),
+				new Usuario("Sebastian", "Pineda", "sebastianp@gmail.com"));
+		return usuarios;
+		
 	}
 
 }
